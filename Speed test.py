6666f1ef2a3b9a72911bb983d3092554a95ce71f
fermartv <fernandomartv@gmail.com>
2019-03-23 23:35:44 +0100

    Speed curve and documentation

--- Speed test.py
+++ Speed test.py
@@ -2,8 +2,8 @@ from Wheel import Rear_Wheel
 from Front_Wheels import Front_Wheels
 import time
 
-speed = 100  #Values from 0 to 100
-wait = 2.2   
+speed = 20  #Values from 0 to 100
+wait = 35
 debug = False
 
 left_PCAchannel = 5  #Values from 0 to 15
@@ -12,12 +12,12 @@ right_PCAchannel = 4  #Values from 0 to 15
 right_pin = 27 #Left wheel = 17; right wheel = 27
 front_PCAchannel = 0
 
-front_wheels = Front_Wheels(front_PCAchannel, offset = -5, debug = debug)
+front_wheels = Front_Wheels(front_PCAchannel, offset = -4, debug = debug)
 left_wheel = Rear_Wheel(left_PCAchannel, left_pin, debug = debug)
 right_wheel = Rear_Wheel(right_PCAchannel, right_pin, debug = debug)
 
-print('Starting in 2 seconds')
-time.sleep(2)
+print('Starting in 3 seconds')
+time.sleep(3)
 
 
 front_wheels.turn_straight()
@@ -30,10 +30,10 @@ left_wheel.stop()
 right_wheel.stop()
 time.sleep(0.5)
 
-left_wheel.backwards(speed)
-right_wheel.backwards(speed)
+left_wheel.backwards(100)
+right_wheel.backwards(100)
 print('Moving backward for ', wait, ' seconds')
-time.sleep(wait)
+time.sleep(2.5)
 
 left_wheel.stop()
 right_wheel.stop()
